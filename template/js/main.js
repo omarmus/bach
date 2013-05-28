80c03c4afc0efbe3d270d73730dbf7d95506241a
Omar Gutierrez <omargc.mus@gmail.com>
2013-05-28 02:14:43 -0400

    Cambios en el template base

--- template/js/main.js
+++ template/js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     $("#slide-toggle-bar").on("click", function () {
-    	var nav = $("#main-nav");
-    	nav.animate({marginTop: nav.css("marginTop") == "0px"?"-50px":0}, function () {
-    		$("#slide-toggle-bar strong").animate({opacity: nav.css("marginTop") == "0px"?0:1}, 100);
+    	var header = $("#header"), 
+    		show = header.css("marginTop") == "0px",
+    		layer1 = show ? $("#nav-left") : header,
+    		layer2 = show ? header : $("#nav-left"),
+    		body = $("body");
+    	layer1.animate(show ? {marginRight: "-100px"} : {marginTop: 0}, function () {
+			layer2.animate(show ? {marginTop: "-50"} : {marginRight: 0});
+    		$("#slide-toggle-bar strong").animate({opacity: show ? 1 : 0});
+    		body.animate(show ? {paddingTop: "20px"} : {paddingRight: "100px"});
     	});
-    	$("body").animate({paddingTop: nav.css("marginTop") == "0px"?"10px":"60px"});
+    	body.animate(show ? {paddingRight: 0} : {paddingTop: "70px"});
     })
 });
