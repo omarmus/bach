7b90b69b0d71efccb4c0e9ced13552359d3e86ba
Omar Gutierrez <omargc.mus@gmail.com>
2013-05-30 20:43:39 -0400

    Change of base template

--- template/js/main.js
+++ template/js/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
-    $("#slide-toggle-bar").on("click", function () {
-    	var header = $("#header"), 
-    		show = header.css("marginTop") == "0px",
-    		layer1 = show ? $("#nav-left") : header,
-    		layer2 = show ? header : $("#nav-left"),
-    		body = $("body");
-    	layer1.animate(show ? {marginRight: "-100px"} : {marginTop: 0}, function () {
-			layer2.animate(show ? {marginTop: "-50"} : {marginRight: 0});
-    		$("#slide-toggle-bar strong").animate({opacity: show ? 1 : 0});
-    		body.animate(show ? {paddingTop: "20px"} : {paddingRight: "100px"});
-    	});
-    	body.animate(show ? {paddingRight: 0} : {paddingTop: "70px"});
-    })
+	showNavs();
+    $("#slide-toggle-bar").on("click", showNavs)
 });
+
+var showNavs = function() {
+	var header = $("#header"), 
+		show = header.css("marginTop") == "0px",
+		layer1 = show ? $("#nav-left") : header,
+		layer2 = show ? header : $("#nav-left"),
+		body = $("body");
+	layer1.animate(show ? {marginRight: "-79px"} : {marginTop: 0}, function () {
+		layer2.animate(show ? {marginTop: "-50px"} : {marginRight: 0});
+		$("#slide-toggle-bar strong").animate({opacity: show ? 1 : 0});
+		body.animate(show ? {paddingTop: "20px"} : {paddingRight: "80px"});
+	});
+	body.animate(show ? {paddingRight: 0} : {paddingTop: "70px"});
+}
