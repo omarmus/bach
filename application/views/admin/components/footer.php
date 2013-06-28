8bf891287325c3d418aaccf26fceaffe180c757c
Omar Gutierrez <omargc.mus@gmail.com>
2013-06-28 18:06:47 -0400

    Datatable RWD

--- application/views/admin/components/footer.php
+++ application/views/admin/components/footer.php
@@ -4,14 +4,15 @@
             <p>&copy; Bach PHP <?php echo date('Y') ?> - Disponible en <?php echo anchor('https://github.com/ozielmus/bach', 'Github') ?></p>
         </footer>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="<?php echo site_url('js/jquery-1.9.1.min.js') ?>"><\/script>')</script>
-        <script src="<?php echo site_url('js/jquery.easing.1.3.js') ?>"></script>
+        <script>window.jQuery || document.write('<script src="<?php echo site_url('lib/jquery/jquery-1.9.1.min.js') ?>"><\/script>')</script>
+        <script src="<?php echo site_url('lib/jquery.easing/jquery.easing.1.3.js') ?>"></script>
         <script type="text/javascript">      
             $(window).load(function() { 
                 $('.spinner').animate({'opacity':0},1000,'easeOutCubic',function (){$(this).css('display','none')});  
             });                         
         </script>
-        <script src="<?php echo site_url('js/bootstrap.min.js') ?>"></script>
+        <script src="<?php echo site_url('lib/bootstrap/js/bootstrap.min.js') ?>"></script>
+        <script src="<?php echo site_url('lib/datatables/js/jquery.dataTables.min.js') ?>"></script>
         <script src="<?php echo site_url('js/main.js') ?>"></script>
     </body>
 </html>
